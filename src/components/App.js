7069747ab5a525e2b2a462fd0a0014300d4d5cb0
Noor Alrai <ameinnoor91@gmail.com>
2023-10-25 13:57:17 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, { useEffect, useState } from 'react';
+import { data } from '../mocks/data';
+
+function App(){
+ const[dogImageUrl , setDogImageUrl] = useState(data.message);
+ const[Loading , setLoading] = useState(true)
+useEffect(()=> {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(r => r.json())
+    .then((data) => {
+        setDogImageUrl(data.message)
+        setLoading(false)})
+    .catch((error) => console.error(error));
+
+}, [])
+
+return (<div className="App">
+{ !Loading ? (
+     <img src={dogImageUrl} alt="A Random Dog" />
+ 
+) : (
+    <p>Loading...</p>
+)}
+</div>)
+}
+
+
+export default App
